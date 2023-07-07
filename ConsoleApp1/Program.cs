e1fb769b3975f916b04f8038f572cc22115769fb
Abdullah <Abdullah@DESKTOP-9007I4A>
2023-07-06 19:46:30 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////SearchArray();
+            //Console.WriteLine("\n\n");
+            ////Palindrome();
+            //Console.WriteLine("\n\n");
+            //unsortedArray();
+            //Console.WriteLine("\n\n");
+            //Triangle();
+            reverseString();
+            Console.Read();
+        }
+
+      
+
+        static void SearchArray()
+        {
+            int[] items =  { 11, 22, 33, 44, 55 };
+            Console.WriteLine("enter a number ");
+            int num1 = Convert.ToInt32( Console.ReadLine());
+            int i = 0;
+            bool found = true;
+            while(i< items.Length)
+            {
+                if(items[i] == num1)
+                {
+                    Console.WriteLine("item found");
+                    found = false;
+                    break;
+
+                }
+                i++;
+            }
+            if (found == true)
+            Console.WriteLine("item not found");
+        }
+
+        static void Palindrome()
+        {
+            Console.WriteLine("enter string to check if it is Palindrome: ");
+            string item = Console.ReadLine();
+            int left = 0, right = item.Length - 1;
+            bool check = true;
+            while(left < right)
+            {
+                if(item[left] != item[right])
+                {
+                    check = false;
+                }
+
+                left++;
+                right--;
+            }
+            if(check== true)
+            {
+                Console.WriteLine("string is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not palindrome");
+            }
+
+
+            
+        }
+
+        static void unsortedArray()
+        {
+            int[] unsortedArray = { 7, 2, 5, 1, 9, 3 };
+
+            int smallest = unsortedArray[0];
+            int largest = unsortedArray[0];
+
+            foreach (int num in unsortedArray)
+            {
+                if (num < smallest)
+                {
+                    smallest = num;
+                }
+
+                if (num > largest)
+                {
+                    largest = num;
+                }
+            }
+
+            Console.WriteLine("Smallest number: " + smallest);
+            Console.WriteLine("Largest number: " + largest);
+
+            
+
+        }
+
+        static void Triangle()
+        {
+            int rows = 5;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void reverseString()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            string reversed = "";
+            int length = input.Length;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+            Console.WriteLine("Reversed string: " + reversed);
+
+        }
+    }
+}
